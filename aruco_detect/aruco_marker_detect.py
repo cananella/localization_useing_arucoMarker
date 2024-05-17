5f093f8d35938289349bedfb66354789349429e7
Cananella <mango7460@naver.com>
2024-05-17 10:00:43 +0900

    5/17

--- aruco_detect/aruco_marker_detect.py
+++ aruco_detect/aruco_marker_detect.py
@@ -8,38 +8,50 @@ def detect_markers(image, camera_matrix, dist_coeffs, marker_size):
     parameters = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
     corners, ids, _ = detector.detectMarkers(image)
-
+    detect_data = []
     if ids is not None:
         cv2.aruco.drawDetectedMarkers(image, corners, ids)
         rvecs, tvecs, _ = my_estimatePoseSingleMarkers(corners, marker_size, camera_matrix, dist_coeffs)
+        
         if rvecs is not None and tvecs is not None:
             for rvec, tvec, marker_id in zip(rvecs, tvecs, ids):
-                rvect, _ = cv2.Rodrigues(rvec)
-                marker_pos = np.dot(-rvect.T, tvec)
-                marker_pos = marker_pos.flatten()
-                print("Marker ID:", marker_id, "Marker position:", marker_pos)
-                print("rvec: ",rvec,"   tvec: ",tvec)
-            print("")
-                
+                rot_mat, _ = cv2.Rodrigues(rvec)
+                yaw, pitch, roll = rotationMatrixToEulerAngles(rot_mat)
+                marker_pos = np.dot(-rot_mat.T, tvec).flatten()
+                # Calculate distance from the camera to the marker
+                distance = np.linalg.norm(tvec)
+                # print(f"Marker ID: {marker_id}, Position: {marker_pos}, Angles: (Yaw: {yaw}, Pitch: {pitch}, Roll: {roll}), Distance: {distance:.2f}m")
+                detect_data.append([marker_id, marker_pos, (yaw, pitch, roll), distance])
     else:
-        print("No markers detected")
-    return image
+        # print("No markers detected")
+        pass
+    return image, detect_data
 
 def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
     marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
                               [marker_size / 2, marker_size / 2, 0],
                               [marker_size / 2, -marker_size / 2, 0],
                               [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
-    trash = []
     rvecs = []
     tvecs = []
     for c in corners:
-        nada, R, t = cv2.solvePnP(marker_points, c,mtx, distortion, False, cv2.SOLVEPNP_IPPE_SQUARE)
+        _, R, t = cv2.solvePnP(marker_points, c, mtx, distortion, False, cv2.SOLVEPNP_IPPE_SQUARE)
         rvecs.append(R)
         tvecs.append(t)
-        trash.append(nada)
-    return rvecs, tvecs, trash
+    return rvecs, tvecs, []
 
+def rotationMatrixToEulerAngles(R):
+    sy = np.sqrt(R[0,0] * R[0,0] + R[1,0] * R[1,0])
+    singular = sy < 1e-6
+    if not singular:
+        x = np.arctan2(R[2,1], R[2,2])
+        y = np.arctan2(-R[2,0], sy)
+        z = np.arctan2(R[1,0], R[0,0])
+    else:
+        x = np.arctan2(-R[1,2], R[1,1])
+        y = np.arctan2(-R[2,0], sy)
+        z = 0
+    return np.degrees(x), np.degrees(y), np.degrees(z)
 
 def load_camera_parameters(yaml_file):
     with open(yaml_file, 'r') as f:
@@ -48,12 +60,13 @@ def load_camera_parameters(yaml_file):
         dist_coeffs = np.array(data["distortion_coefficients"]["data"], dtype=np.float32)
     return camera_matrix, dist_coeffs
 
-
 def main(marker_size=0.105, frame=np.array([0])):
-    camera_matrix, dist_coeffs = load_camera_parameters('calibration_params.yaml')
-    frame = detect_markers(frame, camera_matrix, dist_coeffs, marker_size)
+    camera_matrix, dist_coeffs = load_camera_parameters('agv_localization/calibration_params.yaml')
+    frame, detect_data = detect_markers(frame, camera_matrix, dist_coeffs, marker_size)
+    print(detect_data)
     cv2.imshow('Detected Markers', frame)
-    cv2.waitKey(1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Detect ArUco markers.')
--- aruco_detect/test.py
+++ aruco_detect/test.py
@@ -1,13 +1,89 @@
+import matplotlib.axes
 from socket_video_server import VideoStreamServer
 import aruco_marker_detect
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.animation import FuncAnimation
+import threading
 
-VS=VideoStreamServer()
-VS.accept_connection() 
-camera_matrix, dist_coeffs = aruco_marker_detect.load_camera_parameters('calibration_params.yaml')
-while True:
-    frame=VS.receive_frame()
-    frame = aruco_marker_detect.detect_markers(frame, camera_matrix, dist_coeffs, 0.105)
-    cv2.imshow('Detected Markers', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def aruco_detec_thread():
+    global measurements
+    video_server.accept_connection()
+    video_server.cam_ui = True
+    camera_matrix, dist_coeffs = aruco_marker_detect.load_camera_parameters('agv_localization/calibration_params.yaml')
+    while True:
+        video_server.getframe()
+        detectdata = []
+        measurements = []
+        frame, detectdata = aruco_marker_detect.detect_markers(video_server.frame, camera_matrix, dist_coeffs, 0.053)
+        for id, marker_pos, rot, distance in detectdata:
+            measurements.append((id, distance*100))
+        cv2.imshow('IMG', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+def update_belief(world, marker_positions, measurements, noise=10):
+    """Belief 업데이트 함수"""
+    if not measurements:
+        return world
+    new_world = np.zeros_like(world)
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            prob = 1.0
+            for idx, z in measurements:
+
+                distance = np.sqrt((i - marker_positions[idx[0]][0])**2 + (j - marker_positions[idx[0]][1])**2)
+                prob *= np.exp(-((distance - z)**2) / (2 * noise**2))
+            new_world[i, j] = prob
+    new_world *= world
+    return new_world / new_world.sum()
+
+def plot_world(ax, world, marker_positions):
+    """월드 상태 플로팅"""
+    ax.clear()
+    im = ax.imshow(world, cmap='Blues')
+    scatter_markers = [ax.scatter([pos[1]], [pos[0]], color='red', label='Markers') for pos in marker_positions]  # 마커 위치
+    # 최대 확률 위치 찾기 및 표시
+    max_prob_position = np.unravel_index(np.argmax(world), world.shape)
+    scatter_max_prob = ax.scatter([max_prob_position[1]], [max_prob_position[0]], color='yellow', edgecolors='black', label='Highest Probability')
+    ax.set_title("Histogram Filter Localization")
+    ax.set_xlabel("X position")
+    ax.set_ylabel("Y position")
+    ax.legend()
+    return [im, scatter_max_prob] + scatter_markers
+
+def animate(frame):
+    global world
+
+    print('measurements: ', measurements)
+    if measurements:
+        print('world update')
+        world = update_belief(world, marker_positions, measurements)
+
+    return plot_world(ax, world, marker_positions)
+
+
+
+video_server = VideoStreamServer(host='', port=8888)
+measurements = []
+# 마커의 위치 설정 (여러 개의 마커)
+marker_positions = [(0, 0), (50, 0), (0, 0), (0, 0), (0, 0), (0, 0), (50, 50), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+
+detect_thread = threading.Thread(target=aruco_detec_thread, daemon=True)
+detect_thread.start()
+
+# 환경 설정
+grid_size = (500, 500)  # 격자의 크기
+world = np.zeros(grid_size)  # 확률 분포를 위한 격자 초기화
+
+# 초기 위치 확률 분포 설정
+world += 1
+world /= world.sum()
+
+fig, ax = plt.subplots()
+ani = FuncAnimation(fig, animate, blit=True)
+
+plt.show()
